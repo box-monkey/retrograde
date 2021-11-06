089cda94e7262a29477e189845b13c3743fdd482
box-monkey <jr.gasparin15@gmail.com>
2021-11-06 05:40:46 -0400

    created variables

---
+++ script.js
@@ -0,0 +1,15 @@
+// assign code here
+
+// Global variables delarations here
+var passwordLengthMin = "8";
+var passwordLengthMax = "128";
+var passwordLength = passwordLengthMin
+var characters = "";
+var password = "";
+
+// boolean selectors // true or false things
+
+var useSpecialCharacters = false;
+var useNumericalCharacters = false;
+var useLowerCaseCharacters = false;
+var useUpperCaseCharacters = false;
